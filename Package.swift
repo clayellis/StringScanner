a6b1d98b570bd235e7cea3338fba39a3b3b639cb
Clay Ellis <clayellis@me.com>
2017-09-27 11:52:35 -0600

    Update package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,14 @@
 // swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "StringScanner",
+    products: [
+        .library(name: "StringScanner", targets: ["StringScanner"])
+    ],
     targets: [
-        .target(
-            name: "StringScanner",
-            path: "Sources")
-    ]
+        .target(name: "StringScanner")
+    ],
+    swiftLanguageVersions: [4]
 )
